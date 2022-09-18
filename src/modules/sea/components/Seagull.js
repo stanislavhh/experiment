e2090606b854a5c830a0e09d7af4e1f661e6d22c
shonc <sh996999@gmail.com>
2022-09-18 23:14:57 +0200

    seagull sound

--- src/modules/sea/components/Seagull.js
+++ src/modules/sea/components/Seagull.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF, useAnimations } from '@react-three/drei';
 import SeagullModel from 'assets/models/seagull.glb';
+import useAudio from 'hooks/useAudio';
 import { INITIAL_BIRD_ROTATION, BIRD_SPEED } from 'modules/sea/constants';
+import seagullSound from 'assets/audio/seagull.mp3';
 
 const ANIMATION_NAME = 'ArmatureAction.006';
 
@@ -10,20 +12,28 @@ function Seagull() {
   const seagull = useRef();
   const { scene, animations } = useGLTF(SeagullModel);
   const { actions } = useAnimations(animations, scene);
+  const { track, loaded } = useAudio({ url: seagullSound });
+  track.current.volume = 0.1;
+  const [played, setPlayed] = useState(false)
 
   React.useEffect(() => {
     actions?.[ANIMATION_NAME]?.play();
   });
 
   useFrame(() => {
-    const action = actions[ANIMATION_NAME]
+    const action = actions[ANIMATION_NAME];
 
     if (action.time > 3.5 && action.time < 6.5) {
-      seagull.current.position.y -= 0.01
-      seagull.current.rotation.x += 0.002
-    } else if (action.time >=6.5) {
-      seagull.current.position.y += 0.01
-      seagull.current.rotation.x -= 0.002
+      seagull.current.position.y -= 0.01;
+      seagull.current.rotation.x += 0.002;
+
+      if (!played && loaded) {
+        track.current.play()
+        setPlayed(true)
+      }
+    } else if (action.time >= 6.5) {
+      seagull.current.position.y += 0.01;
+      seagull.current.rotation.x -= 0.002;
     }
 
     seagull.current.position.x += 1 * BIRD_SPEED;
